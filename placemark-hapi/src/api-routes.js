8f16413cc5da50e426afe9315b29f45c9a12e5d5
Dionis <dionis.krasniqi@outlook.de>
2022-06-04 10:50:40 +0200

    added userApi

---
+++ placemark-hapi/src/api-routes.js
@@ -0,0 +1,8 @@
+import { userApi } from "./api/userApi.js";
+
+export const apiRoutes = [
+  { method: "GET", path: "/api/users", config: userApi.find },
+  { method: "GET", path: "/api/users/{id}", config: userApi.findOne },
+  { method: "POST", path: "/api/users", config: userApi.create },
+  { method: "DELETE", path: "/api/users", config: userApi.deleteAll },
+];
